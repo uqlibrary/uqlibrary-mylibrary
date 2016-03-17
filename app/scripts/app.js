c896284a35dd15e4debd26e40618dd827f40b097
Jan-Willem Wisgerhof <jwwisgerhof@gmail.com>
2016-03-18 07:53:22 +1000

    Removed toast, fixed Manifest, removed sw-import

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -11,13 +11,6 @@
     // app.baseUrl = '/polymer-starter-kit/';
   }
 
-  app.displayInstalledToast = function() {
-    // Check to make sure caching is actually enabled—it won't be in the dev environment.
-    if (!Polymer.dom(document).querySelector('platinum-sw-cache').disabled) {
-      Polymer.dom(document).querySelector('#caching-complete').show();
-    }
-  };
-
   // Listen for template bound event to know when bindings
   // have resolved and content has been stamped to the page
   app.addEventListener('dom-change', function() {

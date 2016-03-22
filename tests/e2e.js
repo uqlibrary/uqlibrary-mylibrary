a1e09d2bf3c3ae6b7ad48112b84fb228f50aea95
Jan-Willem Wisgerhof <jwwisgerhof@gmail.com>
2016-03-23 09:49:22 +1000

    Fixed nightwatch test

--- tests/e2e.js
+++ tests/e2e.js
@@ -1,10 +1,10 @@
 module.exports = {
-  'load uqlibrary index page' : function (client) {
+  'load uqlibrary-mylibrary app' : function (client) {
     client
         .url('http://localhost:5001')
         .pause(5000)
         .waitForElementVisible('body', 1000)
-        .assert.elementPresent('uqlibrary-menu', 'uqlibrary-menu component is present')
+        .assert.elementPresent('uqlibrary-app-menu', 'uqlibrary-app-menu component is present')
         .end();
   }
 };

1555942c8621139694ed4b202e46a7f9ff3d5360
Jan-Willem Wisgerhof <jwwisgerhof@gmail.com>
2016-03-23 10:20:46 +1000

    Added test for iron-pages and the uqlibrary-app-menu

--- tests/e2e.js
+++ tests/e2e.js
@@ -1,10 +1,12 @@
 module.exports = {
-  'load uqlibrary-mylibrary app' : function (client) {
+  'load uqlibrary-mylibrary app': function (client) {
     client
-        .url('http://localhost:5001')
-        .pause(5000)
-        .waitForElementVisible('body', 1000)
-        .assert.elementPresent('uqlibrary-app-menu', 'uqlibrary-app-menu component is present')
-        .end();
+      .url('http://localhost:5001')
+      .pause(5000)
+      .waitForElementVisible('body', 1000)
+      .assert.elementPresent('uqlibrary-app-menu', 'uqlibrary-app-menu component is present')
+      .assert.visible('#home')
+      .assert.containsText('uqlibrary-app-menu paper-menu a.iron-selected', 'My Library')
+      .end();
   }
 };

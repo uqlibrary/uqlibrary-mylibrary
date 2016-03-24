f5aed8c0f7ce4fee80992a015085be42ea91756b
Jan-Willem Wisgerhof <jwwisgerhof@gmail.com>
2016-03-24 10:30:03 +1000

    Added base href to My Library

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -16,7 +16,7 @@
     if (window.location.port === '') {  // if production
       // Uncomment app.baseURL below and
       // set app.baseURL to '/your-pathname/' if running from folder in production
-      // app.baseUrl = '/polymer-starter-kit/';
+      app.baseUrl = '<DeploymentUrl>';
     }
 
     // Listen for template bound event to know when bindings

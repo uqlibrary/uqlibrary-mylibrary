e2b87334b3a8dbad3ae614dc53620e0d05229bf7
Jan-Willem Wisgerhof <jwwisgerhof@gmail.com>
2016-05-05 16:07:03 +1000

    Added "Services" link to homepage

--- app/elements/uqlibrary-app-buttons/uqlibrary-app-buttons.js
+++ app/elements/uqlibrary-app-buttons/uqlibrary-app-buttons.js
@@ -78,11 +78,89 @@
         }
       }
 
+      // Add service apps
+      var serviceApps = this._getServiceItems();
+      _.forEach(serviceApps, function (value) {
+        value.isExternal = true;
+        value.isLink = true;
+
+        apps.push(value);
+      });
+
       if (apps.length != this.applications.length) {
         this.applications = apps;
       }
 
       this.fire('uqlibrary-app-buttons-loaded', this.applications);
+    },
+    /**
+     * Returns an app item
+     * @private
+     */
+    _getServiceItems: function () {
+      var student = {
+        link: 'https://www.library.uq.edu.au/library-services/services-for-students',
+        icon: 'hardware:developer-board',
+        title: 'Services for students'
+      };
+      var researchers = {
+        link: 'https://www.library.uq.edu.au/library-services/services-for-researchers',
+        icon: 'hardware:developer-board',
+        title: 'Services for researchers'
+      };
+      var alumni = {
+        link: 'https://www.library.uq.edu.au/library-services/services-for-uq-alumni',
+        icon: 'hardware:developer-board',
+        title: 'Services for UQ alumni'
+      };
+      var secondarySchools = {
+        link: 'https://www.library.uq.edu.au/library-services/services-for-secondary-schools',
+        icon: 'hardware:developer-board',
+        title: 'Services for secondary schools'
+      };
+      var community = {
+        link: 'https://www.library.uq.edu.au/library-services/services-for-community',
+        icon: 'hardware:developer-board',
+        title: 'Services for community'
+      };
+      var hospitalStaff = {
+        link: 'https://www.library.uq.edu.au/library-services/services-for-hospital-staff',
+        icon: 'hardware:developer-board',
+        title: 'Services for hospital staff'
+      };
+
+      var serviceItems = [];
+      switch(this._account.type) {
+        case 1:
+        case 11:
+        case 21:
+        case 31:
+          serviceItems = [ student ];
+          break;
+        case 2:
+        case 22:
+          serviceItems = [ researchers ];
+          break;
+        case 4:
+        case 14:
+          serviceItems = [ alumni ];
+          break;
+        case 7:
+          serviceItems = [ secondarySchools ];
+          break;
+        case 8:
+          serviceItems = [ community ];
+          break;
+        case 9:
+          serviceItems = [ hospitalStaff ];
+          break;
+        case 17:
+        case 18:
+          serviceItems = [ student, researchers, secondarySchools, community, hospitalStaff, alumni ];
+          break;
+      }
+
+      return serviceItems;
     }
   })
 })();

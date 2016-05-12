401c902b2654ee134108cb0577d55f3fef15d052
Jan-Willem Wisgerhof <jwwisgerhof@gmail.com>
2016-05-12 12:26:11 +1000

    Updated nightwatch url

--- tests/e2e.js
+++ tests/e2e.js
@@ -1,7 +1,7 @@
 module.exports = {
   'load uqlibrary-mylibrary app': function (client) {
     client
-      .url('http://localhost:5001')
+      .url('http://localhost:5001/demo.html')
       .pause(5000)
       .waitForElementVisible('body', 1000)
       .assert.elementPresent('uqlibrary-menu', 'uqlibrary-menu component is present')

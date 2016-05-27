537cd0e634e154a02e1687a060fb3f404f0f5117
Cliff Lien <c.lien@library.uq.edu.au>
2016-05-27 15:55:16 +1000

    clean up code to display app buttons

--- app/elements/uqlibrary-app-buttons/uqlibrary-app-buttons.js
+++ app/elements/uqlibrary-app-buttons/uqlibrary-app-buttons.js
@@ -89,18 +89,14 @@
 
           if (item.link.indexOf("http") == 0) {
             item.isExternal = true;
-
-            if (item.title === 'Document delivery' || item.title === 'Feedback') {
-              apps.push(item);
-            }
           } else {
             item.isExternal = false;
             item.route = item.link.substring(1);
             item.href = item.route;
+          }
 
-            if (item.route != "home") {
-              apps.push(item);
-            }
+          if (item.route !== "home") {
+            apps.push(item);
           }
         }
       }

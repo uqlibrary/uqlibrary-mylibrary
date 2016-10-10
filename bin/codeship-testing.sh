78c9d8f6992295e071bab83d7277cbcc349c3da9
Lea de Groot <l.degroot@library.uq.edu.au>
2016-10-10 12:01:52 +1000

    add tests to mylibrary [PT #130802129]

--- bin/codeship-testing.sh
+++ bin/codeship-testing.sh
@@ -37,13 +37,34 @@ case "$branch" in
     echo "install selenium"
     curl -sSL https://raw.githubusercontent.com/codeship/scripts/master/packages/selenium_server.sh | bash -s
     cd bin/local
+
+    echo "test firefox (default)"
     ./nightwatch.js
+
+    echo "test chrome"
     ./nightwatch.js --env chrome
   ;;
   "3")
     cd bin/saucelabs
+
+    echo "test chrome on windows (default)"
     ./nightwatch.js
-    ./nightwatch.js --env ie11
+
+    echo "test edge"
+    ./nightwatch.js --env edge
+
+    echo "test firefox on windows"
+    ./nightwatch.js --env firefox-on-windows
+
+    echo "test chrome on mac"
+    ./nightwatch.js --env chrome-on-mac
+
+    echo "test firefox on mac"
+    ./nightwatch.js --env firefox-on-mac
+
+    echo "test safari on mac"
+    ./nightwatch.js --env safari-on-mac
+
   ;;
   esac
   ;;

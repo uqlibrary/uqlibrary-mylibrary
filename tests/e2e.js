573f7c1407dfb550e196fc19c59999a8c286528b
Ky <k.lane@uq.edu.au>
2017-06-01 17:08:14 +1000

    Fixing tests

--- tests/e2e.js
+++ tests/e2e.js
@@ -2,8 +2,8 @@ module.exports = {
   'load uqlibrary-mylibrary app': function (client) {
     client
       .url('http://localhost:5001/demo.html')
-      .pause(5000)
-      .waitForElementVisible('body', 1000)
+      .pause(10000)
+      .waitForElementVisible('body', 20000)
       .assert.elementPresent('uqlibrary-menu', 'uqlibrary-menu component is present')
       .assert.visible('#home')
       .assert.containsText('uqlibrary-menu paper-menu a.iron-selected', 'My Library')

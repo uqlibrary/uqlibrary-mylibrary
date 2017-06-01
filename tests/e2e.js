05bf7543fdffde063a26541b4b6c2eee6a8e1ecb
Ky <k.lane@uq.edu.au>
2017-06-02 09:39:37 +1000

    Fixing tests

--- tests/e2e.js
+++ tests/e2e.js
@@ -4,9 +4,10 @@ module.exports = {
       .url('http://localhost:5001/demo.html')
       .pause(10000)
       .waitForElementVisible('body', 20000)
+      .waitForElementVisible('uqlibrary-menu', 20000)
       .assert.elementPresent('uqlibrary-menu', 'uqlibrary-menu component is present')
       .assert.visible('#home')
       .assert.containsText('uqlibrary-menu paper-menu a.iron-selected', 'My Library')
-      .end();
+      .end(); done();
   }
 };
